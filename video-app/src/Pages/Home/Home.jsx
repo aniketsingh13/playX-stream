6ffada7dd1a9533eb856bf80350ff600bcf94a6e
aniketsingh13 <85512784+aniketsingh13@users.noreply.github.com>
2022-05-16 00:23:32 +0530

    style: use prettier

--- video-app/src/Pages/Home/Home.jsx
+++ video-app/src/Pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Navbar from "../../Component/Navbar/Navbar";
-import {AiOutlineGithub,AiOutlineTwitter} from "react-icons/ai";
+import { AiOutlineGithub, AiOutlineTwitter } from "react-icons/ai";
 
 import "./Home.css";
 const Home = () => {
@@ -33,19 +33,31 @@ const Home = () => {
           </div>
         </div>
         <div className="categoriesType_container ">
-          <img src="https://res.cloudinary.com/aniket-singh/image/upload/v1652637547/Images/l-intro-1645209424_msay4q.jpg" className="category_img" alt="controller" />
+          <img
+            src="https://res.cloudinary.com/aniket-singh/image/upload/v1652637547/Images/l-intro-1645209424_msay4q.jpg"
+            className="category_img"
+            alt="controller"
+          />
           <div className="categroies_nameDiv p-xss">
             <h3 className="categories_Name f-m font-xl p-xss">Controller</h3>
           </div>
         </div>
         <div className="categoriesType_container ">
-          <img src="https://res.cloudinary.com/aniket-singh/image/upload/v1652638383/Images/https___hybrismediaprod.blob.core.windows.net_sys-master-phoenix-images-container_hd3_h57_9081222660126_RZ04-02670100-R3U1-0_yxptea.jpg" className="category_img" alt="accessories" />
+          <img
+            src="https://res.cloudinary.com/aniket-singh/image/upload/v1652638383/Images/https___hybrismediaprod.blob.core.windows.net_sys-master-phoenix-images-container_hd3_h57_9081222660126_RZ04-02670100-R3U1-0_yxptea.jpg"
+            className="category_img"
+            alt="accessories"
+          />
           <div className="categroies_nameDiv p-xss">
             <h3 className="categories_Name f-m font-xl p-xss">Accesssories</h3>
           </div>
         </div>
         <div className="categoriesType_container ">
-          <img src="https://res.cloudinary.com/aniket-singh/image/upload/v1652639014/Images/dYpWGqygVSVet396ZyNHWZ_ek2ecn.jpg" className="category_img" alt="console" />
+          <img
+            src="https://res.cloudinary.com/aniket-singh/image/upload/v1652639014/Images/dYpWGqygVSVet396ZyNHWZ_ek2ecn.jpg"
+            className="category_img"
+            alt="console"
+          />
           <div className="categroies_nameDiv p-xss">
             <h3 className="categories_Name f-m font-xl p-xss">Console</h3>
           </div>
@@ -54,8 +66,20 @@ const Home = () => {
       <footer className="category_footer mt-l mb-m">
         <h3 className="f-s font-l">made with 💗 by Aniket Singh</h3>
         <div className="f-m p-s">
-        <a href="https://github.com/aniketsingh13" target="_blank" className="fotter_icons mr-m"><AiOutlineGithub/></a>
-        <a href="https://twitter.com/ANIKETS42266714" target="_blank"  className="fotter_icons"><AiOutlineTwitter /></a>
+          <a
+            href="https://github.com/aniketsingh13"
+            target="_blank"
+            className="fotter_icons mr-m"
+          >
+            <AiOutlineGithub />
+          </a>
+          <a
+            href="https://twitter.com/ANIKETS42266714"
+            target="_blank"
+            className="fotter_icons"
+          >
+            <AiOutlineTwitter />
+          </a>
         </div>
       </footer>
     </div>
