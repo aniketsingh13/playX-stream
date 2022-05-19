5441c0fbec2d158b9dc23bbaa8ce1318c9c4ca73
aniketsingh13 <85512784+aniketsingh13@users.noreply.github.com>
2022-05-19 23:36:46 +0530

    feat: 404 page

--- video-app/src/Component/SingleVideoCard/SingleVideoCard.jsx
+++ video-app/src/Component/SingleVideoCard/SingleVideoCard.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./SingleVideoCard.css";
-import {BiLike} from "react-icons/bi";
-import {RiPlayListAddFill} from "react-icons/ri";
-import {MdOutlineWatchLater} from "react-icons/md"
+import { BiLike } from "react-icons/bi";
+import { RiPlayListAddFill } from "react-icons/ri";
+import { MdOutlineWatchLater } from "react-icons/md";
 
 const SingleVideoCard = ({ videos }) => {
-  const { _id, title, views, avatar, description, creatorName,alt } = videos;
+  const { _id, title, views, avatar, description, creatorName, alt } = videos;
   return (
     <div className="singleVideo_card">
       <iframe
@@ -17,20 +17,40 @@ const SingleVideoCard = ({ videos }) => {
       ></iframe>
       <h3 className="f-m font-l mt-s">{title}</h3>
       <div className="singleVideo_center flex mt-l">
-          <div className="flex ">
-          <img src={ avatar} alt={alt}  className='singlePage_avatar'/>
+        <div className="flex ">
+          <img src={avatar} alt={alt} className="singlePage_avatar" />
           <div>
-              <h4 className="p-xss ml-s f-s font-xl">{creatorName}</h4>
-              <h4 className="p-xss ml-s f-s font-xl">{views}</h4>
-              </div>
+            <h4 className="p-xss ml-s f-s font-xl">{creatorName}</h4>
+            <h4 className="p-xss ml-s f-s font-xl">{views}</h4>
+          </div>
+        </div>
+        <div className="flex">
+          <div className="flex mr-l">
+            <BiLike className="f-s " />
+            <span className="singlepage_iconText ml-s f-s font-xl mb-s">
+              {" "}
+              LIKE
+            </span>
+          </div>
+          <div className="flex mr-l">
+            <RiPlayListAddFill className="f-s " />
+            <span className="singlepage_iconText ml-s f-s font-xl mb-s">
+              {" "}
+              SAVE TO PLAYLIST
+            </span>
           </div>
           <div className="flex">
-          <div className="flex mr-l"><BiLike className="f-s " /><span className="singlepage_iconText ml-s f-s font-xl mb-s"> LIKE</span></div>
-          <div className="flex mr-l"><RiPlayListAddFill className="f-s " /><span className="singlepage_iconText ml-s f-s font-xl mb-s"> SAVE TO PLAYLIST</span></div>
-          <div className="flex"><MdOutlineWatchLater className="f-s " /><span className="singlepage_iconText ml-s f-s font-xl mb-s"> WATCH LATER</span></div>
+            <MdOutlineWatchLater className="f-s " />
+            <span className="singlepage_iconText ml-s f-s font-xl mb-s">
+              {" "}
+              WATCH LATER
+            </span>
           </div>
+        </div>
       </div>
-      <p className="mt-l mb-l f-s font-m singlePage_description">{description}</p>
+      <p className="mt-l mb-l f-s font-m singlePage_description">
+        {description}
+      </p>
     </div>
   );
 };
--- video-app/src/Routes/RoutePath.jsx
+++ video-app/src/Routes/RoutePath.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {Routes,Route} from "react-router-dom"
 import Mockman from "mockman-js";
 import Home from "../Pages/Home/Home";
-import { SinglePage, VideoListing } from '../Pages';
+import { Page404, SinglePage, VideoListing } from '../Pages';
 
 
   const RoutePath = () => {
@@ -12,6 +12,7 @@ import { SinglePage, VideoListing } from '../Pages';
           <Route path='/' element={<Home />} />
           <Route path='/videolisting' element={<VideoListing />} />
           <Route path='/videolisting/:Id'   element={<SinglePage />} />
+          <Route path='*' element={<Page404 />} />
           <Route path='/mockman' element={<Mockman />} />
           </Routes>
       </div>
